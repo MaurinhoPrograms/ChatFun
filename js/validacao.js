11cd6cfa9320f9ee00318809d011c082411d2bf7
Luiran Junior <luiran.junior@escolar.ifrn.edu.br>
2026-05-08 16:21:16 -0300

    Versão 1.2

---
+++ js/validacao.js
@@ -0,0 +1,22 @@
+const usuarios = ['mimmarcelo', 'Luiran', 'Dayvid', 'Mauro', 'Lucas'];
+const senhas = ['Teste123', '123', '123', '123', '123'];
+
+function validacao() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    var contador = 0;
+    var validador = 0;
+    while(contador < usuarios.length || validador == 0) {
+        if(username == usuarios[contador] && password == senhas[contador]) {
+            validador++;
+        }
+        contador++;
+    }
+
+    if(validador == 1) {
+        return alert(`Seja bem-vindo, ` + username);
+    } else {
+        return alert('Desculpe... Esse usuário não existe!');
+    }
+}
